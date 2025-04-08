2483895abcbc6a25bc53cb4ebfd1b7d95d7e2898
aherreramilet <aherrera@dl.cl>
2025-04-08 13:26:46 -0300

    ajustado slider

--- src/components/slider-service.tsx
+++ src/components/slider-service.tsx
@@ -1,9 +1,10 @@
 "use client"
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {Pagination} from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules"; // 1. Importa Autoplay
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/autoplay'; // 2. Importa el CSS de autoplay (opcional)
 import { serviceData } from '@/data';
 
 export default function SliderService() {
@@ -27,12 +28,17 @@ export default function SliderService() {
             pagination={{
                 clickable: true
             }}
-            modules={[Pagination]}
+            autoplay={{ // 3. Configura autoplay
+                delay: 2500, // 2.5 segundos entre transiciones
+                disableOnInteraction: false // Continúa autoplay después de interacción
+            }}
+            loop={true} // 4. Activa el loop infinito
+            modules={[Pagination, Autoplay]} // 5. Añade Autoplay a los módulos
             className="h-[280px] md:h-[390px] w-[270px] md:w-[650px]"
         >
          {serviceData.map((item, index) => (
                 <SwiperSlide key={index} >
-                    <div className="flex px-6 py-8 h-auto md:h-[290px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-accent border-2">
+                    <div className="flex px-6 py-8 h-auto md:h-[240px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-accent border-2">
                         <div className="mb-4 text-4xl text-accent">{item.icon}</div>
                         <div className='text-white overflow-y-auto'>
                             <h3 className="mb-4 text-lg">{item.title}</h3>
